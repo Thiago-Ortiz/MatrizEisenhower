7b7810689327abeab9e8444689ad6fcbbf655372
Thiago Ortiz de Almeida <80494447+Thiago-Ortiz@users.noreply.github.com>
2023-10-02 01:10:20 -0300

    add remove tasks

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod/dist/zod'
 import "./AddTask.css"
 
 type TFormSchema = {
+  id: number;
   task: string,
   description: string,
   important: boolean,
@@ -28,11 +29,36 @@ export default function AddTask(){
 
   })
 
+  function RemoveTask(id, list){
+    if(list == 1)(
+    setDoTasks(current => current.filter(task => {
+      return task.id !== id;
+    }))
+    )
+    else if(list == 2)(
+      setScheduleTasks(current => current.filter(task => {
+        return task.id !== id;
+    }))
+    )
+    else if(list == 3)(
+      setDelegateTasks(current => current.filter(task => {
+        return task.id !== id;
+    }))
+    )
+    else if(list == 4)(
+      setDeleteTasks(current => current.filter(task => {
+        return task.id !== id;
+    }))
+    )
+    
+  }
+
   const {register, handleSubmit, watch} = useForm<TFormSchema>({
     resolver: zodResolver(validationSchema)
   })
 
   const onSubmit: SubmitHandler<TFormSchema> = (data, e) => {
+    data.id = Math.floor(Math.random() * 100000)
     if(data.important == true && data.urgent == true){ 
     e?.preventDefault()
     e?.stopPropagation()
@@ -63,32 +89,15 @@ export default function AddTask(){
     return(
     <>
     
-      <form onSubmit={handleSubmit(onSubmit)}>
-
-        <label> Task <input {...register("task")} type="task"/> </label> <br />
-
-        <label> Description <input {...register("description")} type="description"/> </label> <br />
-
-        
-        <label> <input {...register("important")} type="checkbox"/> Important</label> <br />
-
-        <label> <input {...register("urgent")} type="checkbox"/> Urgent</label> <br />
-
-        <button type="submit"> Submit </button>
-
-        </form>
-
-
+    <div className="wrapper">
     <section className="DoNow">
       <h1>DO NOW</h1>
-    {
-      DoTasks.map((task, index )=>(
-      <div key={index}>
-      <h2>{task.task}</h2>
-      <p>{task.description}</p>
-      <p>{task.important}</p>
-      <p>{task.urgent}</p>
       <hr />
+    {
+      DoTasks.map((task)=>(
+        <div className="mostrar" key={task.id}>
+        <h2>{task.task}: </h2> <h3>{task.description} </h3>
+        <button onClick={() => RemoveTask(task.id, 1)}>remove task</button>
       </div>
       ))
     }
@@ -96,14 +105,12 @@ export default function AddTask(){
 
     <section className="Schedule">
       <h1>SCHEDULE</h1>
-    {
-      ScheduleTasks.map((task, index )=>(
-      <div key={index}>
-      <h2>{task.task}</h2>
-      <p>{task.description}</p>
-      <p>{task.important}</p>
-      <p>{task.urgent}</p>
       <hr />
+    {
+      ScheduleTasks.map((task)=>(
+        <div className="mostrar" key={task.id}>
+        <h2>{task.task}: </h2> <h3>{task.description}</h3>
+        <button onClick={() => RemoveTask(task.id, 2)}>remove task</button>
       </div>
       ))
     }
@@ -111,14 +118,12 @@ export default function AddTask(){
 
     <section className="Delegate">
       <h1>DELEGATE</h1>
-    {
-      DelegateTasks.map((task, index)=>(
-      <div key={index}>
-      <h2>{task.task}</h2>
-      <p>{task.description}</p>
-      <p>{task.important}</p>
-      <p>{task.urgent}</p>
       <hr />
+    {
+      DelegateTasks.map((task)=>(
+        <div className="mostrar" key={task.id}>
+        <h2>{task.task}: </h2> <h3>{task.description}</h3>
+        <button onClick={() => RemoveTask(task.id, 3)}>remove task</button>
       </div>
       ))
     }
@@ -126,18 +131,25 @@ export default function AddTask(){
 
     <section className="Delete">
       <h1>DELETE</h1>
-    {
-      DeleteTasks.map((task, index )=>(
-      <div key={index}>
-      <h2>{task.task}</h2>
-      <p>{task.description}</p>
-      <p>{task.important}</p>
-      <p>{task.urgent}</p>
       <hr />
+    {
+      DeleteTasks.map((task)=>(
+        <div className="mostrar" key={task.id}>
+        <h2>{task.task}: </h2> <h3>{task.description}</h3>
+        <button onClick={() => RemoveTask(task.id, 4)}>remove task</button>
       </div>
       ))
     }
     </section>
+    </div>
+
+  <form className="form" onSubmit={handleSubmit(onSubmit)}>
+  <label> Task <input {...register("task")} type="task"/> </label> <br />
+  <label> Description <input {...register("description")} type="description"/> </label> <br />
+  <label> <input {...register("important")} type="checkbox"/> Important</label> <br />
+  <label> <input {...register("urgent")} type="checkbox"/> Urgent</label> <br />
+  <button type="submit"> Submit </button>
+  </form>
 
     </>
     )
